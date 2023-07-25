2a86d859b3b9bb3b2d78e7d37f0b21e455740d70
SidKalje <sidkalje123@gmail.com>
2023-07-24 19:02:04 -0700

    making request to server

--- client/learngpt/src/components/GenCoursePage.tsx
+++ client/learngpt/src/components/GenCoursePage.tsx
@@ -1,26 +1,30 @@
 import React, { useState } from "react";
+import "../styles/GenCoursePageStyles.css";
 import axios from "axios";
 
 function GenCoursePage() {
   const [GPTResponse, setGPTResponse] = useState("");
-
+  const [input, setInput] = useState("");
   const getGPTResponse = async () => {
-    const prompt = "your-prompt-goes-here"; //replace this with your actual prompt
-    try {
-      const response = await axios.post("http://localhost:5000/api/gpt", {
-        prompt: prompt,
-      });
-      console.log(response.data);
-      setGPTResponse(response.data);
-    } catch (error) {
-      console.error(error);
-    }
+    const prompt = `I want to learn this concept in chemistry: ${input}. Create a roadmap of steps I can take to learn that concept. Make each step no more than 100 words. At the end of each step, put a "#". Then, for that step, give me specific keywords, each started with a $, like "$motion\\", which I can put into youtube to get sample videos explaining that concept. Avoid any repetition between steps, and if a step is not necessary, there is no need to include it. Do not repeat keywords in steps.`;
+    const response = await axios.post("/api/gpt", { prompt: prompt });
+    console.log(response.data);
+    setGPTResponse(response.data);
   };
 
   return (
     <div>
       GenCoursePage
-      <button onClick={getGPTResponse}>Generate Text</button>
+      <button onClick={getGPTResponse} className="standardButton">
+        Generate Text
+      </button>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
       <div>{GPTResponse}</div>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const axios = require("axios");
 
-require("dotenv").config();
+require("dotenv").config(); // Configuring dot env to load env variables
 const apiKey = process.env.OPENAI_API_KEY;
 const app = express();
+
+app.use(express.json())
 
 const client = axios.create({
   headers: {
@@ -15,10 +17,12 @@ app.get("/api", (req, res) => {
   res.json({ users: ["user1", "user2"] });
 });
 
-app.get("/api/gpt", async (req, res) => {
+app.post("/api/gpt", async (req, res) => {
+  const prompt = req.body.prompt;
+  console.log(prompt);
   const params = {
-    prompt:
-      'I want to learn this concept in chemistry: ionic bonds. Create a roadmap of steps I can take to learn that concept. Make each step 100 words. At the end of each step, put a "#". Then, for that step, give me specific keywords, each started with a $, like "$motion", which I can put into youtube to get sample videos explaining that concept. ',
+    prompt: prompt
+      ,
     model: "text-davinci-003",
     max_tokens: 1000,
     temperature: 0,
@@ -27,12 +31,12 @@ app.get("/api/gpt", async (req, res) => {
     .post("https://api.openai.com/v1/completions", params)
     .then((result) => {
       const resultArray = result.data.choices[0].text.split("#");
-      const finalResultArray = result Array.map((element) => element.split("$"));
-      res.send(resultArray);
+      const finalResultArray = resultArray.map((element) => element.split("$"));
       res.send(finalResultArray);
     })
     .catch((err) => {
       console.log(err);
+      res.send(err);
     });
 });
 
